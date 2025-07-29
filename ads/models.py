888ef72b7cf9e8e41ae2a0ab0c3ba7418641e788
Ammar Chaudhry <ammarsc02@gmail.com>
2025-07-28 22:17:41 -0400

    ad object

---
+++ ads/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from firebase_auth.models import User
+
+class Ad(models.Model):
+
+    AD_TYPES = [
+        ('videographer', 'Videographer'),
+        ('photographer', 'Photographer'),
+        ('snowRemoval', 'Snow Removal'),
+        ('automotive', 'Automotive'),
+        ('moving', 'Moving'),
+        ('websiteDesign', 'Website Design'),
+    ]
+
+    # Ad fields
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    is_available_now = models.BooleanField(default=False)
+    ad_type = models.CharField(choices = AD_TYPES)
+    is_active = models.BooleanField(default=True)
+
+    # Photos
+    photo_1 = models.ImageField(upload_to='ads/photos/', blank=True, null=True)
+    photo_2 = models.ImageField(upload_to='ads/photos/', blank=True, null=True)
+    photo_3 = models.ImageField(upload_to='ads/photos/', blank=True, null=True)
+
+    # Search optimization fields
+    location = models.CharField(max_length=200, blank=True, help_text="City / General Area")
+    tags = models.CharField(max_length = 200, blank=True, help_text="Comma-separated tags like: wedding (for photography), blog (for website)")
+    skills = models.CharField(max_length = 200, blank=True, help_text="Comma-separated skills like: Photoshop, Django, etc.")
+
+    # User relationship
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['tags']),
+            models.Index(fields=['location']),
+            models.Index(fields=['ad_type']),
+            models.Index(fields=['is_available_now'])
+        ]
+    
+    def __str__(self):
+        return f"{self.title} by {self.user.name}"
+    
+    def get_photos(self):
+        photos = []
+        for photo in [self.photo_1, self.photo_2, self.photo_3]:
+            if photo:
+                photos.append(photo)
+        return photos
+
+    
--- contractingo/settings.py
+++ contractingo/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'firebase_auth'
+    'firebase_auth',
+    'ads'
 ]
 
 MIDDLEWARE = [
